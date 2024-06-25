c0fee6e5c73201417963b7ca9320641d51c40824
Merrcurys <a.sebezhko2017@gmail.com>
2024-06-26 01:33:20 +0300

    Добавил VM и авторизацию за пользователя

--- pr10/ViewModel/Helpers/ApiHelper.cs
+++ pr10/ViewModel/Helpers/ApiHelper.cs
@@ -11,13 +11,13 @@ namespace pr10.ViewModel.Helpers
     {
         private static string URL = "http://localhost:5023/api/";
 
-        public static string Put(string json, int id, string tableName)
+        public static string Put(string json, long id, string tableName)
         {
             try
             {
                 HttpClient client = new HttpClient();
                 HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-                HttpResponseMessage message = client.PutAsync($"{URL}/{tableName}/{id}", content).Result;
+                HttpResponseMessage message = client.PutAsync($"{URL}{tableName}/{id}", content).Result;
                 return message.Content.ReadAsStringAsync().Result;
             }
             catch (Exception ex)
@@ -31,7 +31,7 @@ namespace pr10.ViewModel.Helpers
             try
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage message = client.GetAsync($"{URL}/{tableName}").Result;
+                HttpResponseMessage message = client.GetAsync($"{URL}{tableName}").Result;
                 return message.Content.ReadAsStringAsync().Result;
             }
             catch (Exception ex)
@@ -40,12 +40,12 @@ namespace pr10.ViewModel.Helpers
             }
         }
 
-        public static string GetById(int id, string tableName)
+        public static string GetById(long id, string tableName)
         {
             try
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage message = client.GetAsync($"{URL}/{tableName}/{id}").Result;
+                HttpResponseMessage message = client.GetAsync($"{URL}{tableName}/{id}").Result;
                 return message.Content.ReadAsStringAsync().Result;
             }
             catch (Exception ex)
@@ -60,7 +60,7 @@ namespace pr10.ViewModel.Helpers
             {
                 HttpClient client = new HttpClient();
                 HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-                HttpResponseMessage message = client.PostAsync($"{URL}/{tableName}", content).Result;
+                HttpResponseMessage message = client.PostAsync($"{URL}{tableName}", content).Result;
                 return message.Content.ReadAsStringAsync().Result;
             }
             catch (Exception ex)
@@ -69,13 +69,12 @@ namespace pr10.ViewModel.Helpers
             }
         }
 
-
-        public static string Delete(int id, string tableName)
+        public static string Delete(long id, string tableName)
         {
             try
             {
                 HttpClient client = new HttpClient();
-                HttpResponseMessage message = client.DeleteAsync($"{URL}/{tableName}/{id}").Result;
+                HttpResponseMessage message = client.DeleteAsync($"{URL}{tableName}/{id}").Result;
                 return message.Content.ReadAsStringAsync().Result;
             }
             catch (Exception ex)
--- pr10/ViewModel/UserAutoViewModel.cs
+++ pr10/ViewModel/UserAutoViewModel.cs
@@ -1,14 +1,62 @@
-﻿using pr10.ViewModel.Helpers;
+﻿using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using pr10.Model;
+using pr10.View;
+using pr10.ViewModel.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace pr10.ViewModel
 {
-    internal class UserAutoViewModel: BindingHelper
+    internal class UserAutoViewModel : BindingHelper
     {
+        private string _policyNumber;
+        private Patients _currentPatient;
 
+        public string PolicyNumber
+        {
+            get => _policyNumber;
+            set
+            {
+                _policyNumber = value;
+                OnPropertyChanged(nameof(PolicyNumber));
+            }
+        }
+
+        public Patients CurrentPatient
+        {
+            get => _currentPatient;
+            set
+            {
+                _currentPatient = value;
+                OnPropertyChanged(nameof(CurrentPatient));
+            }
+        }
+
+        public ICommand AuthenticateCommand { get; }
+
+        public UserAutoViewModel()
+        {
+            AuthenticateCommand = new RelayCommand(Authenticate);
+        }
+
+        private void Authenticate()
+        {
+            try
+            {
+                var patientData = ApiHelper.GetById(Convert.ToInt64(PolicyNumber), "Patient");
+                var CurrentPatient = JsonConvert.SerializeObject(patientData);
+
+                UserWindow window = new UserWindow();
+                Application.Current.MainWindow.Close();
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{CurrentPatient} Пользователь не найден", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
     }
 }
